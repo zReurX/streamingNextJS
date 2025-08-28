e3ea3ef438b26c21a8964a0a7c475c0c13e78aeb
Roberto <robertodonini8@gmail.com>
2025-08-28 16:10:16 +0200

    aggiunta speed insights

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +32,8 @@ export default function RootLayout({ children }) {
             disableTransitionOnChange
           >
             {children}
+
+            <SpeedInsights />
           </ThemeProvider>
       </body>
     </html>
